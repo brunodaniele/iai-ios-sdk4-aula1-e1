aade78fe71e0a5faf13d50f2bd0f03832d805527
Bruno Daniele <brunodaniele@gmail.com>
2014-09-01 19:35:35 -0300

    Initial Commit

---
+++ CelulaCustomizada/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CelulaCustomizada
+//
+//  Created by Bruno Daniele on 9/1/14.
+//  Copyright (c) 2014 Bruno Daniele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CelulaCustomizada/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CelulaCustomizada
+//
+//  Created by Bruno Daniele on 9/1/14.
+//  Copyright (c) 2014 Bruno Daniele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
